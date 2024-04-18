0a484cafa5477f8a88a20d2ceb3d550b03ca7960
measternmage <foodyatt800@gmail.com>
2024-04-18 12:56:11 +0200

    first commit

---
+++ p5.js
@@ -0,0 +1,16 @@
+function setup(){
+    createCanvas(500, 1000);
+    background(255);
+    fill(0);
+    textSize(30);
+    text('To Do List', 200, 100);
+}
+
+function listContainer(){
+    fill(211,211,211);
+   rect(0, 150, 500, 700);
+}
+
+function draw(){
+    listContainer();
+}
