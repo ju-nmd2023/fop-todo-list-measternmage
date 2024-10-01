02795a6866182bfca49aff50648cbbe7514cabc7
measternmage <foodyatt800@gmail.com>
2024-10-01 16:02:57 +0200

    display tasks done

--- p5.js
+++ p5.js
@@ -6,23 +6,39 @@ let inputBox, addButton;
 function setup() {
   createCanvas(width, height);
   background(150);
+
+  
   inputBox = createInput();
   inputBox.position(500, 800); 
 
+  
   addButton = createButton('Add Task');
-  addButton.position(700, 800);
+  addButton.position(700,800);
   addButton.mousePressed(addTask); 
+
+  
+  textAlign(LEFT, TOP);
+  textSize(24);
+  fill(0);
 }
 
-function draw() {
-  background(150); 
-  displayTasks(); 
-} 
+function displayTasks() {
+    let startY = 50; 
+    for (let i = 0; i < tasks.length; i++) {
+      text((i + 1) + '. ' + tasks[i], 500, startY + i * 30); 
+    }
+  }
 
 function addTask() {
   let task = inputBox.value(); 
   if (task) {
     tasks.push(task); 
-    inputBox.value('');
+    inputBox.value(''); 
   }
 }
+  function draw() {
+    background(150); 
+    displayTasks(); 
+  } 
+  
+  
