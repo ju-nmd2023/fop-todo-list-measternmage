98b8cbc651e29dd4d05e2435c5a948459114edb8
measternmage <foodyatt800@gmail.com>
2025-05-22 23:25:42 +0200

    done and delete tasks

--- todo.js
+++ todo.js
@@ -11,13 +11,33 @@ function addTask() {
 }
 
 function renderTasks() {
-  const list = document.getElementById('taskList');
-  list.innerHTML = "";
-  tasks.forEach((task) => {
-    const li = document.createElement('li');
-    const span = document.createElement('span');
-    span.textContent = task.text;
-    li.appendChild(span);
-    list.appendChild(li);
-  });
-}
+    const list = document.getElementById('taskList');
+    list.innerHTML = "";
+    tasks.forEach((task, index) => {
+      const li = document.createElement('li');
+      if (task.done) li.classList.add('done');
+  
+      const span = document.createElement('span');
+      span.textContent = task.text;
+      span.onclick = () => toggleTask(index);
+  
+      const delBtn = document.createElement('button');
+      delBtn.textContent = "Delete";
+      delBtn.onclick = () => deleteTask(index);
+  
+      li.appendChild(span);
+      li.appendChild(delBtn);
+      list.appendChild(li);
+    });
+  }
+  
+  function toggleTask(index) {
+    tasks[index].done = !tasks[index].done;
+    renderTasks();
+  }
+  
+  function deleteTask(index) {
+    tasks.splice(index, 1);
+    renderTasks();
+  }
+  
